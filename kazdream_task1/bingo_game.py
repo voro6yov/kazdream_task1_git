1391494e23dc1de8d9400ee683138eac1a386b07
Valentin Vorobyov <voro6yov.valentin@gmail.com>
2020-08-06 14:39:50 +0600

    окончательная версия

--- kazdream_task1/bingo_game.py
+++ kazdream_task1/bingo_game.py
@@ -1,3 +1,6 @@
+"""
+Module contains BingoGame class.
+"""
 from typing import List
 from random import choice
 
@@ -5,13 +8,25 @@ from utils import Observer, Subject
 from bingo_player import BingoPlayer
 
 
+RANGE_BEGIN: int = 1
+RANGE_END: int = 100
+
+
 class BingoGame(Subject):
     """
-    Игра в Бинго. У каждого игрока есть билетик с 5 случайными числами(числа от 1 до 99). 
-    Игра случайно выбирает число от 1 до 99 и оповещает игроков об этом числе. Игрок, который 
-    первым собрал свою комбинацию, должен выкрикнуть свое имя и игра остановится.
+    Bingo game. Each player has a ticket with 5 random numbers (numbers from 1 to 99).
+    The game randomly chooses a number between 1 and 99 and notifies the players about
+    this number. The player who first collected his combination must call out his name
+    and the game will stop.
     """
+
     def __init__(self, player_prototype: BingoPlayer) -> None:
+        """
+        Class constructor.
+
+        Args:
+            player_prototype (BingoPlayer): player prototype, used to create players
+        """
         self._player_prototype = player_prototype
         self._observers: List[Observer] = []
         self._number = 0
@@ -19,16 +34,35 @@ class BingoGame(Subject):
 
     @property
     def number(self) -> int:
+        """
+        Getter for the number attribute.
+
+        Returns:
+            int: value of number attribute
+        """
         return self._number
 
     @property
     def game_state(self) -> bool:
+        """
+        Getter for the game state attribute.
+
+        Returns:
+            bool: current game state
+        """
         return self._game_state
 
     @game_state.setter
     def game_state(self, a_state: bool) -> None:
+        """
+        Setter for the game state attribute.
+
+        Args:
+            a_state (bool): current game state
+        """
         self._game_state = a_state
 
+    # Observer pattern methods
     def attach(self, observer: 'Observer') -> None:
         self._observers.append(observer)
 
@@ -40,30 +74,48 @@ class BingoGame(Subject):
             observer.update(self)
 
     def initialize_players(self, player_names: List[str]) -> None:
+        """
+        Initializes Bingo players.
+
+        Args:
+            player_names (List[str]): list of player names
+        """
         for player_name in player_names:
+            # Initialize prototype
             self._player_prototype.initialize(player_name)
+            # Clone player prototype
             a_player: BingoPlayer = self._player_prototype.clone()
+            # Subscribe player for updates
             self.attach(a_player)
 
     def start_game(self) -> None:
-        numbers_in_the_game: List[int] = list(range(1, 100))
+        """
+        Starts the Bingo game.
+        """
+        # Initilize Bingo game sample of the numbers
+        numbers_in_the_game: List[int] = list(range(RANGE_BEGIN, RANGE_END))
+        # Print welcome message
         print('Welcome to the Bingo game!')
+        # While winner is not determinated
         while self._game_state:
+            # Pick a random number from the sample
             self._number = choice(numbers_in_the_game)
+            # Remove the number from the sample
             numbers_in_the_game.remove(self._number)
+            # Notify players about the number
             print(f'We got number: {self._number}')
             self.notify()
 
 
-
 if __name__ == '__main__':
     philosopher_names: List[str] = [
-        'Thomas Aquinas', 
-        'Aristotle', 
-        'Confucius', 
-        'René Descartes', 
+        'Thomas Aquinas',
+        'Aristotle',
+        'Confucius',
+        'René Descartes',
         'Ralph Waldo Emerson'
-        ]
+    ]
+
     a_game = BingoGame(BingoPlayer())
     a_game.initialize_players(philosopher_names)
     a_game.start_game()
--- kazdream_task1/bingo_player.py
+++ kazdream_task1/bingo_player.py
@@ -1,5 +1,5 @@
 """
-Module c
+Module contains BingoPlayer class.
 """
 from random import sample
 from copy import deepcopy
@@ -8,76 +8,83 @@ from typing import List
 from utils import Observer, Subject
 
 
+RANGE_BEGIN: int = 1
+RANGE_END: int = 100
+DIGITS_NUMBER: int = 5
+
+
 class BingoPlayer(Observer):
     """
-    Игрок в Бинго. У игрока есть имя и билетик с 5 случайными числами( числа от 1 до 99). Для
-    создания новых игроков используется паттерн Прототип.
+    Bingo player. The player has a name and a ticket with 5 random numbers (numbers from 1 to 99).
+    The Prototype pattern is used to create new players. The Observer pattern is used to notify
+    player about current Bingo number.
     """
 
     def __init__(self) -> None:
         """
-        Конструктор класса. При инициализации прототипа объявляются закрытые атрибуты класса,
-        с присвоением им значений None.
+        Class constructor.
         """
+        # Initialize private attributes
         self._name: str = None
         self._ticket: List[int] = None
 
     @property
     def name(self) -> str:
         """
-        Getter для доступа к атрибуту name.
+        Getter of the name attribute.
 
         Returns:
-            str: имя игрока
+            str: player name
         """
         return self._name
 
     @property
     def ticket(self) -> List[int]:
         """
-        Getter для доступа к атрибуту ticket.
+        Getter of the ticket attribute.
 
         Returns:
-            List[int]: билетик игрока
+            List[int]: player's ticket
         """
         return self._ticket
 
     def initialize(self, name: str) -> None:
         """
-        Инициализирует прототип перед его клонированием.
+        Initializes the prototype before cloning it.
 
         Args:
-            name (str): имя игрока
+            name (str): player name
         """
-        # Присвоить игроку имя
+        # Assign player name
         self._name = name
-        # Сформировать билетик с пятью случайными числами в диапазоне от 1 до 99
-        self._ticket = sample(range(1, 100), 5)
+        # Generate a ticket with five random numbers ranging from 1 to 99
+        self._ticket = sample(range(RANGE_BEGIN, RANGE_END), DIGITS_NUMBER)
 
     def clone(self) -> 'BingoPlayer':
         """
-        Клонирует прототип игрока. Перед вызовом метода обязательно необходимо инициализировать
-        прототип.
+        Clones a prototype player. The prototype must be initialized before calling the method.
 
         Returns:
-            BingoPlayer: игрок в бинго
+            BingoPlayer: a bingo player
         """
         return deepcopy(self)
 
     def update(self, subject: Subject) -> None:
+        """
+        Updates player's information about current Bingo number.
+
+        Args:
+            subject (Subject): Bingo game
+        """
+        # Get current Bingo number
         current_number = subject.number
+        # If current Bingo number in ticket
         if current_number in self._ticket:
+            # Remove it from ticket
             self._ticket.remove(current_number)
+            # If ticket doesn't has numbers
             if self._ticket == []:
+                # Print winner message
                 print(f'{self._name.upper()} BINGO!!!')
+                # Finish the Bingo game
                 subject.game_state = False
-
-
-
-if __name__ == '__main__':
-    player_prototype = BingoPlayer()
-
-    for a_name in ['Valentin', 'Egor']:
-        player_prototype.initialize(a_name)
-        a_player = player_prototype.clone()
-        print(f"Player: {a_player.name} has ticket {a_player.ticket}")
